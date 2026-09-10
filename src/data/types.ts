bb836ce7f77282bb80d026d808d64d1da5c75ec3
MUMBA-Amos <mumbantambo@gmail.com>
2026-09-10 14:54:25 +0800

    feat: add product and response types

---
+++ src/data/types.ts
@@ -0,0 +1,10 @@
+export interface ApiProduct{
+    id: number;
+    title: string;
+    price: number;
+    thumbnail: string;
+    images: string[];
+    description: string;
+    rating: number;
+}
+
